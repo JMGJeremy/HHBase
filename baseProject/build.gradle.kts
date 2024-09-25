0f98d5173bedd751c0d46560693e79e5656512e6
Jeremy Kruid <kruid9403@msn.com>
2024-09-25 09:40:21 -0500

    Large text

--- baseProject/build.gradle.kts
+++ baseProject/build.gradle.kts
@@ -111,7 +111,7 @@ publishing{
         register<MavenPublication>("release"){
             groupId = "com.jmgjeremy"
             artifactId = "hhbase"
-            version = "1.2.15"
+            version = "1.2.16"
             afterEvaluate{
                 from(components["release"])
             }
