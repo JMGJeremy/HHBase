6658a69617d7aef5dff36a17af510edbcac84273
Jeremy Kruid <kruid9403@msn.com>
2024-09-26 15:56:39 -0500

    Standard text handling

--- baseProject/build.gradle.kts
+++ baseProject/build.gradle.kts
@@ -111,7 +111,7 @@ publishing{
         register<MavenPublication>("release"){
             groupId = "com.jmgjeremy"
             artifactId = "hhbase"
-            version = "1.2.16"
+            version = "1.2.21"
             afterEvaluate{
                 from(components["release"])
             }
