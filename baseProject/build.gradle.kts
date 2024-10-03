1e52f38f6a409491a4dcc2571433b6afde96146b
robdf <sdf>
2024-10-03 10:14:13 -0500

    Swipe Screens

--- baseProject/build.gradle.kts
+++ baseProject/build.gradle.kts
@@ -111,7 +111,7 @@ publishing{
         register<MavenPublication>("release"){
             groupId = "com.jmgjeremy"
             artifactId = "hhbase"
-            version = "1.2.21"
+            version = "1.2.31"
             afterEvaluate{
                 from(components["release"])
             }
