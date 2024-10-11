0f4a8f7e761d03677a2966754fd59b95a80ad9ba
robdf <sdf>
2024-10-11 13:56:51 -0500

    Back buttons

---
+++ baseProject/src/main/java/com/jmg/baseproject/ui/images/ChevronBackButton.kt
@@ -0,0 +1,35 @@
+package com.jmg.baseproject.ui.images
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jmg.baseproject.R
+
+@Composable
+fun ChevronBackButton(
+    onClick: () -> Unit
+) {
+    Image(
+        painter = painterResource(R.drawable.chevron_left),
+        contentDescription = "Back",
+        modifier = Modifier
+            .size(24.dp)
+            .clickable(onClick = onClick),
+        colorFilter = ColorFilter.tint(color = MaterialTheme.colorScheme.primary)
+    )
+}
+
+@Preview
+@Composable
+fun PreviewBackButton() {
+    ChevronBackButton(
+        onClick = {}
+    )
+}
